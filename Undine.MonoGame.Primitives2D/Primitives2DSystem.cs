cad057c5b61850d2441b4c9c7e2ffd5b78fc4689
Tomasz Cekało <tomasz.cekalo@gmail.com>
2024-01-03 12:50:48 +0100

    changed SpriteBatch registration + increased package version

--- Undine.MonoGame.Primitives2D/Primitives2DSystem.cs
+++ Undine.MonoGame.Primitives2D/Primitives2DSystem.cs
@@ -8,43 +8,44 @@ namespace Undine.MonoGame.Primitives2D
 {
     public class Primitives2DSystem : UnifiedSystem<Primitives2DComponent, TransformComponent>
     {
-        private SpriteBatch _spriteBatch;
+        private SpriteBatch SpriteBatch { get; set; }
         private Dictionary<Primitives2DDrawType, Action<Primitives2DComponent, TransformComponent>> _primitives2DDrawType;
 
-        public Primitives2DSystem(SpriteBatch spriteBatch)
+        public Primitives2DSystem()
         {
-            _spriteBatch = spriteBatch;
-            _primitives2DDrawType = new Dictionary<Primitives2DDrawType, Action<Primitives2DComponent, TransformComponent>>();
-            _primitives2DDrawType.Add(Primitives2DDrawType.DrawArc, DrawArc);
-            _primitives2DDrawType.Add(Primitives2DDrawType.DrawCircle, DrawCircle);
-            _primitives2DDrawType.Add(Primitives2DDrawType.DrawLine, DrawLine);
-            _primitives2DDrawType.Add(Primitives2DDrawType.DrawRectangle, DrawRectangle);
-            _primitives2DDrawType.Add(Primitives2DDrawType.FillRectangle, FillRectangle);
+            _primitives2DDrawType = new Dictionary<Primitives2DDrawType, Action<Primitives2DComponent, TransformComponent>>
+            {
+                { Primitives2DDrawType.DrawArc, DrawArc },
+                { Primitives2DDrawType.DrawCircle, DrawCircle },
+                { Primitives2DDrawType.DrawLine, DrawLine },
+                { Primitives2DDrawType.DrawRectangle, DrawRectangle },
+                { Primitives2DDrawType.FillRectangle, FillRectangle }
+            };
         }
 
         private void FillRectangle(Primitives2DComponent arg1, TransformComponent arg2)
         {
-            _spriteBatch.FillRectangle(arg2.Position - arg1.Size / 2, arg1.Size, arg1.Color, arg2.Rotation);
+            SpriteBatch.FillRectangle(arg2.Position - arg1.Size / 2, arg1.Size, arg1.Color, arg2.Rotation);
         }
 
         private void DrawRectangle(Primitives2DComponent arg1, TransformComponent arg2)
         {
-            _spriteBatch.DrawRectangle(arg2.Position, arg1.Size, arg1.Color, arg1.Thickness);
+            SpriteBatch.DrawRectangle(arg2.Position, arg1.Size, arg1.Color, arg1.Thickness);
         }
 
         private void DrawLine(Primitives2DComponent arg1, TransformComponent arg2)
         {
-            _spriteBatch.DrawLine(arg2.Position, arg1.Size, arg1.Color, arg1.Thickness);
+            SpriteBatch.DrawLine(arg2.Position, arg1.Size, arg1.Color, arg1.Thickness);
         }
 
         private void DrawCircle(Primitives2DComponent arg1, TransformComponent arg2)
         {
-            _spriteBatch.DrawCircle(arg2.Position, arg1.Size.X, arg1.Sides, arg1.Color, arg1.Thickness);
+            SpriteBatch.DrawCircle(arg2.Position, arg1.Size.X, arg1.Sides, arg1.Color, arg1.Thickness);
         }
 
         private void DrawArc(Primitives2DComponent arg1, TransformComponent arg2)
         {
-            _spriteBatch.DrawArc(arg2.Position, arg1.Size.X, arg1.Sides, arg2.Rotation, arg1.Size.Y, arg1.Color);
+            SpriteBatch.DrawArc(arg2.Position, arg1.Size.X, arg1.Sides, arg2.Rotation, arg1.Size.Y, arg1.Color);
         }
 
         public override void ProcessSingleEntity(int entityId, ref Primitives2DComponent a, ref TransformComponent b)
